32ab2db37c8f3c4fc7e5d6d0f2886c627d05128c
Александр Гринин <112979587+W9JAK@users.noreply.github.com>
2024-04-18 10:36:10 +0300

    Update run.py

--- main/run.py
+++ main/run.py
@@ -1,11 +1,25 @@
+import time
 from threading import Thread
 import main
+from telebot import TeleBot
+
+
+bot = TeleBot(main.my_token)
+
 
 def run_bot():
-    main.bot.polling(none_stop=True)
+    while True:
+        try:
+            print("Запуск бота...")
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
+            time.sleep(10)
 
+# Функция для запуска веб-сервера с использованием waitress
 def run_waitress_server():
     from serve import serve, app
+    print("Запуск веб-сервера...")
     serve(app, host='0.0.0.0', port=8080)
 
 
