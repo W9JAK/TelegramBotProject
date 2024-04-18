8ff95eec3b26bcef066e0cd268de3c897a86ffa2
dmitry <fiodoroff.dmitryi@yandex.ru>
2024-04-18 14:52:28 +0300

    add traceback

--- main/run.py
+++ main/run.py
@@ -1,4 +1,5 @@
 import time
+import traceback
 from threading import Thread
 from main import start_bot
 
@@ -8,7 +9,7 @@ def run_bot():
             print("Запуск бота...")
             start_bot()
         except Exception as e:
-            print(f"Произошла ошибка: {e}")
+            print(f"Произошла ошибка: {e}, {traceback.format_exc()}")
             time.sleep(10)
 
 # Функция для запуска веб-сервера с использованием waitress
