39a350f8c751b6ddd0cdcda9e90dde9e90f5eb18
W9JAK <112979587+W9JAK@users.noreply.github.com>
2024-04-25 23:26:49 +0300

    fix migr

--- migration/202404252248.sql
+++ migration/202404252248.sql
@@ -6,6 +6,7 @@ ADD COLUMN standard_completion_time VARCHAR(255);
 ALTER TABLE orders
 ADD COLUMN partial_payment_completed BOOLEAN DEFAULT FALSE,
 ADD COLUMN payment_status integer DEFAULT 0,
-ADD COLUMN order_status integer DEFAULT 0;
+ADD COLUMN order_status integer DEFAULT 0,
+ADD COLUMN is_visible boolean DEFAULT true;
 
 -- migrate:down
